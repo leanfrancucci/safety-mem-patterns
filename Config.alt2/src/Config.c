7ef5732c6df6b16376b45c4ca7665d38f8857812
Leandro Francucci <lf@vortexmakes.com>
2021-07-29 13:38:03 -0300

    Updated file permissions

--- Config.alt2/src/Config.c
+++ Config.alt2/src/Config.c
@@ -1,5 +1,15 @@
 /*
  * --------------------------------------------------------------------------
+ * MIT License
+ *
+ * Copyright (c) 2021 Leandro Francucci
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  * --------------------------------------------------------------------------
  */
 
@@ -73,15 +83,6 @@ checkDataFromNVMem(Config *data)
         res = true;
     }
     return res;
-}
-
-static bool
-checkData(const Config *data)
-{
-    Crc32 crc;
-
-    crc = Crc32_calc((const uint8_t *)data, sizeof(Config), 0xffffffff);
-    return (crc == data->crc) ? true : false;
 }
 
 /* ---------------------------- Global functions --------------------------- */
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Safety patterns
 This repository contains the source code and unit test cases explainded in 
-the article X.
+the article [Protecting your embedded software against memory corruption](x).
 Config.alt1, Config.alt2 and Config.recovery directories are three 
 alternatives to implement the module Config according to the proposed method.
 Config.alt1 and Config.alt2 are similar but the last one does not check the 
 data set stored in RAM every time a configuration option is accessed by set 
-and get functions. The alternative Config.recovery is derived from 
+and get functions, whereas the alternative Config.recovery is derived from 
 Config.alt2 but includes the recovery mechanism.
